7687012b92252f3bc47384a9ecab194d93fe8fca
geekerstar <247507792@qq.com>
2018-12-01 11:17:38 +0800

    finish 01

---
+++ springboot-01-helloworld-quick/src/main/java/com/geekerstar/springboot/Springboot01HelloworldQuickApplication.java
@@ -0,0 +1,12 @@
+package com.geekerstar.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot01HelloworldQuickApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot01HelloworldQuickApplication.class, args);
+    }
+}
